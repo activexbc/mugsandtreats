e9dd40899ae7582e176e4b1b4164df7b37977042
activexbc <activexbc23@icloud.com>
2023-06-04 17:25:46 +0100

    upgraded

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ import mongoose from "mongoose";
 
 import router from "./router.js";
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-  })
+  .connect(
+    "mongodb+srv://activexbc:<password>@cluster1.yf1ml7a.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
   .then(() => {
     console.log("Connect to DB");
   })
